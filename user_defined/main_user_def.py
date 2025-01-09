4f9424a35b45aa4a98d8491933293365e688ac86
Corey Rice <171101634+RiceC-at-MasonHS@users.noreply.github.com>
2025-01-09 08:13:05 -0500

    performance wrapper, ouput options

--- user_defined/main_user_def.py
+++ user_defined/main_user_def.py
@@ -1,7 +1,11 @@
 # best practice to only import the functions you need from a module
-from ud_palindrome import optimized_palindrome_depth
+from ud_palindrome import optimized_palindrome_depth, performance_wrapper
+from ud_output import print_dict_raw, print_dict_simple, print_dict_sorted_keys, print_dict_sorted_values
 
-min, max = 0, 0
+#generate default values
+min, max = 0, 10
+
+#ask user for range of numbers
 while True:
     try:
         min = int(input("set bottom of range (must be zero or larger): "))
@@ -24,13 +28,32 @@ while True:
 # create and empty dictionary
 all_numbers = {str(i):None for i in range(min,max+1)}
 
-# Fill the dictionary with the palindrome depth of each number
-all_numbers = optimized_palindrome_depth(all_numbers)
+#ask user if they want to measure performance
+while True:
+    measure_performance = input("Do you want to measure performance? (y/n): ")
+    if measure_performance.lower() == 'y':
+        # same operation, but with performance measurement
+        all_numbers = performance_wrapper(optimized_palindrome_depth, all_numbers)
+        break
+    elif measure_performance.lower() == 'n':
+        # Fill the dictionary with the palindrome depth of each number
+        all_numbers = optimized_palindrome_depth(all_numbers)
+        break
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
 
-# Print the dictionary
-print(all_numbers)
-
-# Perhaps more readable output
-for num in all_numbers:
-    print(f"{num}: depth-{all_numbers[num]}")
-    
+# ask the user if they would like output as a raw dictionary, unsorted, sorted by keys, or sorted by values
+while True:
+    output_type = input("How would you like the output? (raw, simple, keys, values): ")
+    if output_type in ['raw', 'simple', 'keys', 'values']:
+        if output_type == 'raw':
+            print_dict_raw(all_numbers)
+        elif output_type == 'simple':
+            print_dict_simple(all_numbers)
+        elif output_type == 'keys':
+            print_dict_sorted_keys(all_numbers)
+        elif output_type == 'values':
+            print_dict_sorted_values(all_numbers)
+        break
+    else:
+        print("Invalid input. Please enter 'raw', 'simple', 'keys', or 'values'.")
